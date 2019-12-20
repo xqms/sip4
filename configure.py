bc8752400c8dfbb6373520f7a6b4660daa7a5b37
Dmitry Shachnev <mitya57@debian.org>
2019-12-20 13:23:38 +0300

    New upstream version 4.19.20+dfsg

--- configure.py
+++ configure.py
@@ -31,8 +31,8 @@ import siputils
 
 
 # Initialise the globals.
-sip_version = 0x041313
-sip_version_str = "4.19.19"
+sip_version = 0x041314
+sip_version_str = "4.19.20"
 py_version = sys.hexversion >> 8
 py_platform = sys.platform
 plat_py_site_dir = None
